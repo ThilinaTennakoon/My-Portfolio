ed9041a4572a477f59b6ea6b6b445080d1de1dda
ThilinaTennakoon <thilinatms2@gmail.com>
2024-06-03 21:28:59 +0530

    try fixed error 1

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Carousel, Typography, Button } from "@material-tailwind/react";
-import Image from 'next/image';
 
 function CarouselSlider() {
   return (
@@ -31,12 +30,10 @@ function CarouselSlider() {
     }
     >
       <div className="relative h-full w-full">
-        <Image
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+        <img
+          src={"slider/Fb1.jpg"}
           alt="image 1"
           className="h-full w-full object-cover"
-          width={1200} // Replace with appropriate width
-          height={800} // Replace with appropriate height
         />
         <div className="absolute inset-0 grid h-full w-1/2 place-items-center bg-black/75">
           <div className="w-3/4 text-center md:w-2/4">
@@ -57,60 +54,7 @@ function CarouselSlider() {
           </div>
         </div>
       </div>
-      <div className="relative h-full w-full">
-        <Image
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-          alt="image 1"
-          className="h-full w-full object-cover"
-          width={1200} // Replace with appropriate width
-          height={800} // Replace with appropriate height
-          />
-        <div className="absolute inset-0 grid h-full w-1/2 place-items-center bg-black/75">
-          <div className="w-3/4 text-center md:w-2/4">
-            <Typography
-              variant="h1"
-              color="white"
-              className="mb-4 text-3xl md:text-4xl lg:text-sm"
-            >
-              The Beauty of Nature
-            </Typography>
-            <Typography
-              variant="lead"
-              color="white"
-              className="mb-12 opacity-80"
-            >
-              It is not so much for its beauty that the forest makes a claim upon
-            </Typography>
-          </div>
-        </div>
-      </div>
-      <div className="relative h-full w-full">
-        <Image
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-          alt="image 1"
-          className="h-full w-full object-cover"
-          width={1200} // Replace with appropriate width
-          height={800} // Replace with appropriate height
-        />
-        <div className="absolute inset-0 grid h-full w-1/2 place-items-center bg-black/75">
-          <div className="w-3/4 text-center md:w-2/4">
-            <Typography
-              variant="h1"
-              color="white"
-              className="mb-4 text-3xl md:text-4xl lg:text-sm"
-            >
-              The Beauty of Nature
-            </Typography>
-            <Typography
-              variant="lead"
-              color="white"
-              className="mb-12 opacity-80"
-            >
-              It is not so much for its beauty that the forest makes a claim upon
-            </Typography>
-          </div>
-        </div>
-      </div>
+     
     </Carousel>
   );
 }
